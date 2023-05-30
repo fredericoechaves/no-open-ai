21cdb699cb0a8e5fea47c15585e266295310225b
Frederico Chaves <frederico.chaves@gmail.com>
2023-05-30 18:42:08 -0300

    Using perf_counter instead of time.time() and ctgas on gitignore

--- profile.py
+++ profile.py
@@ -3,8 +3,8 @@ import sys
 
 class Profile:
 	def __init__(self):
-		self.start_time = time.time()
+		self.start_time = time.perf_counter()
 
 	def log(self, message=""):
-		print(f"{message}{time.time() - self.start_time :.2f}", file=sys.stderr)
-		self.start_time = time.time()
+		print(f"{message}({time.perf_counter() - self.start_time :.2f})", file=sys.stderr)
+		self.start_time = time.perf_counter()
